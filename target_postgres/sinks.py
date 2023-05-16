5c2921842d77989e20c1837896c5434f028dbe72
Dan Norman <buzzcutnorman@gmail.com>
2023-05-16 14:05:33 -0700

    added more ranges of increases

--- target_postgres/sinks.py
+++ target_postgres/sinks.py
@@ -264,8 +264,12 @@ class postgresSink(SQLSink):
 
         if perf_diff < 0:
             self.MAX_SIZE_DEFAULT = self.max_size - 10
+        if perf_diff >= 0.3 and perf_diff < 0.5:
+            self.MAX_SIZE_DEFAULT = self.max_size + 100
+        elif perf_diff >= 0.5 and perf_diff < 0.75:
+            self.MAX_SIZE_DEFAULT = self.max_size + 1000
         elif perf_diff >= 0.75:
-            self.MAX_SIZE_DEFAULT = self.max_size + 10
+            self.MAX_SIZE_DEFAULT = self.max_size + 10000
 
     def conform_name(self, name: str, object_type: Optional[str] = None) -> str:
         """Conform a stream property name to one suitable for the target system.
