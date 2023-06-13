802b269a8ad74dff366f5feb2cd6193abe7505d8
Dan Norman <buzzcutnorman@gmail.com>
2023-06-13 14:42:30 -0700

    added default_connector_class, _target_connector, and add_sqlsink

--- target_postgres/target.py
+++ target_postgres/target.py
@@ -3,10 +3,12 @@
 from __future__ import annotations
 
 from singer_sdk.target_base import SQLTarget
+from singer_sdk.connectors import SQLConnector
 from singer_sdk import typing as th
 
 from target_postgres.sinks import (
     postgresSink,
+    postgresConnector
 )
 
 
@@ -15,6 +17,20 @@ class Targetpostgres(SQLTarget):
 
     name = "target-postgres"
     default_sink_class = postgresSink
+    default_connector_class = postgresConnector
+    _target_connector: SQLConnector = None
+
+    @property
+    def target_connector(self) -> SQLConnector:
+        """The connector object.
+
+        Returns:
+            The connector object.
+        """
+        if self._target_connector is None:
+            self._target_connector = self.default_connector_class(dict(self.config))
+        return self._target_connector
+
 
     config_jsonschema = th.PropertiesList(
         th.Property(
@@ -146,6 +162,90 @@ class Targetpostgres(SQLTarget):
         ),
     ).to_dict()
 
+    def add_sqlsink(
+        self,
+        stream_name: str,
+        schema: dict,
+        key_properties: list[str] | None = None,
+    ):
+        """Create a sink and register it.
 
+        This method is internal to the SDK and should not need to be overridden.
+
+        Args:
+            stream_name: Name of the stream.
+            schema: Schema of the stream.
+            key_properties: Primary key of the stream.
+
+        Returns:
+            A new sink for the stream.
+        """
+        self.logger.info("Initializing '%s' target sink...", self.name)
+        sink_class = self.default_sink_class
+        sink = sink_class(
+            target=self,
+            stream_name=stream_name,
+            schema=schema,
+            key_properties=key_properties,
+            connector=self.target_connector,
+        )
+        sink.setup()
+        self._sinks_active[stream_name] = sink
+        return sink
+    
+    def get_sink(
+        self,
+        stream_name: str,
+        *,
+        record: dict | None = None,
+        schema: dict | None = None,
+        key_properties: list[str] | None = None,
+    ):
+        """Return a sink for the given stream name.
+
+        A new sink will be created if `schema` is provided and if either `schema` or
+        `key_properties` has changed. If so, the old sink becomes archived and held
+        until the next drain_all() operation.
+
+        Developers only need to override this method if they want to provide a different
+        sink depending on the values within the `record` object. Otherwise, please see
+        `default_sink_class` property and/or the `get_sink_class()` method.
+
+        Raises :class:`singer_sdk.exceptions.RecordsWithoutSchemaException` if sink does
+        not exist and schema is not sent.
+
+        Args:
+            stream_name: Name of the stream.
+            record: Record being processed.
+            schema: Stream schema.
+            key_properties: Primary key of the stream.
+
+        Returns:
+            The sink used for this target.
+        """
+        _ = record  # Custom implementations may use record in sink selection.
+        if schema is None:
+            self._assert_sink_exists(stream_name)
+            return self._sinks_active[stream_name]
+
+        existing_sink = self._sinks_active.get(stream_name, None)
+        if not existing_sink:
+            return self.add_sqlsink(stream_name, schema, key_properties)
+
+        if (
+            existing_sink.schema != schema
+            or existing_sink.key_properties != key_properties
+        ):
+            self.logger.info(
+                "Schema or key properties for '%s' stream have changed. "
+                "Initializing a new '%s' sink...",
+                stream_name,
+                stream_name,
+            )
+            self._sinks_to_clear.append(self._sinks_active.pop(stream_name))
+            return self.add_sqlsink(stream_name, schema, key_properties)
+
+        return existing_sink
+    
 if __name__ == "__main__":
     Targetpostgres.cli()
